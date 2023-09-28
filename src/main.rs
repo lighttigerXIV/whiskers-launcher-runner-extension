a996d4d29e954a54a74959f29754e3af9352f5fc
lighttigerxiv <lighttigerxiv@gmail.com>
2023-09-28 21:25:42 +0100

    Updated to new crate

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,16 @@ use std::process::{Command};
 
 use simple_kl_rs::actions::{ExtensionAction, ResultAction};
 use simple_kl_rs::extensions::Function::{GetResults, RunAction};
-use simple_kl_rs::extensions::{emit_results, get_parameters};
+use simple_kl_rs::extensions::{emit_results, get_extension_setting, get_parameters};
 use simple_kl_rs::paths::get_extension_directory;
 use simple_kl_rs::results::{IconWithTextResult, SimpleKLResult};
-use simple_kl_rs::settings::Settings;
+const EXTENSION_ID: &str = "com-lighttigerxiv-terminal-runner";
 
 fn main() {
     let parameters = get_parameters().unwrap();
     let function = parameters.function;
-    let extension_id = "com-lighttigerxiv-terminal-runner";
 
-    let mut icon_path = get_extension_directory(extension_id).unwrap();
+    let mut icon_path = get_extension_directory(EXTENSION_ID).unwrap();
     icon_path.push("src");
     icon_path.push("icons");
     icon_path.push("icon.svg");
@@ -26,28 +25,27 @@ fn main() {
             results.push(SimpleKLResult::IconWithText(
                 IconWithTextResult::new_with_color(
                     icon_path,
-                    "accent",
                     &format!("Run {}", search_text.to_owned()),
                     ResultAction::ExtensionAction(ExtensionAction::new_with_args(
-                        extension_id,
+                        EXTENSION_ID,
                         "",
                         vec![search_text.to_owned()],
                     )),
                 ),
             ));
 
-            emit_results(results);
+            emit_results(&results);
         }
         RunAction => {
             let args = parameters.custom_args.unwrap();
             let command = args.get(0).unwrap().to_owned();
 
-            let terminal = Settings::get_extension_setting(extension_id, "terminal").unwrap();
+            let terminal = get_extension_setting(EXTENSION_ID, "terminal").unwrap();
 
             let execution_parameter =
-                Settings::get_extension_setting(extension_id, "exec").unwrap();
+                get_extension_setting(EXTENSION_ID, "exec").unwrap();
 
-            let shell = Settings::get_extension_setting(extension_id, "shell").unwrap();
+            let shell = get_extension_setting(EXTENSION_ID, "shell").unwrap();
 
             let environemnt_shell = env::var("SHELL").unwrap();
 
